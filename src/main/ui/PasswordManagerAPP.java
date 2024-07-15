23ca9bad9ff5cbc918438fbd7b398c357d9464e6
royary <ruiyangz57@gmail.com>
2024-07-15 15:26:06 -0700

    finish deleteItem user story

--- src/main/ui/PasswordManagerAPP.java
+++ src/main/ui/PasswordManagerAPP.java
@@ -51,8 +51,11 @@ public class PasswordManagerAPP {
     private void displayMenu() {
         System.out.println("\nSelect from");
         System.out.println("\ts -> save password");
+        System.out.println("\td -> delete password");
+        System.out.println("\tg -> generate password");
         System.out.println("\tv -> view item list");
         System.out.println("\tq -> quit");
+
     }
 
     private void runCommand(String command) {
@@ -60,6 +63,10 @@ public class PasswordManagerAPP {
             savePassword();
         } else if (command.equals("v")) {
             viewItemList(newItemList);
+        } else if (command.equals("d")) {
+            deleteItem(newItemList);
+        } else if (command.equals("g")) {
+            generatePassword(newItemList);
         } else {
             System.out.println("Invalid selection...");
         }
@@ -74,14 +81,15 @@ public class PasswordManagerAPP {
         System.out.println("\nInput username:");
         String username = input.next();
         Item newItem = new Item(itemName, password, username);
-        newItemList.add(newItem);    
+        newItemList.add(newItem); 
+        System.out.println("\nNew item successfully created!");   
     }
 
-    private void viewItemList(List<Item> newItemList2) {
+    private void viewItemList(List<Item> newItemList) {
         System.out.println("------------------------------------------------");
         System.out.println("|  Item Name  |  Username  |  Password  |");
         System.out.println("------------------------------------------------");
-        for (Item item : newItemList2) {
+        for (Item item : newItemList) {
             System.out.printf("|  %-10s  |  %-10s  |  %-10s  |%n", 
                     item.getItemName(), item.getUsername(), item.getPassword());
         }
@@ -90,6 +98,66 @@ public class PasswordManagerAPP {
 
 
     }
-       
+    
+    private void deleteItem(List<Item> newItemList) {
+        System.out.println("\nInput item name to delete:");
+        String itemNameToDelete = input.next();
+    
+        int count = countItem(newItemList, itemNameToDelete);
+    
+        if (count == 1) {
+            deleteItemUnique(newItemList, itemNameToDelete);
+        } else if (count > 1) {
+            deleteItemMultiple(newItemList, itemNameToDelete);
+        } else {
+            System.out.println("\nItem not found. Please check the item name and try again.");
+        }
+    }
+
+    public int countItem(List<Item> newItemList, String itemName) {
+        int count = 0;
+        for (Item item : newItemList) {
+            if (item.getItemName().equals(itemName)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public void deleteItemUnique(List<Item> newItemList, String itemName) {
+        for (Item item : newItemList) {
+            if (item.getItemName().equals(itemName)) {
+                newItemList.remove(item);
+                System.out.println("\nItem successfully deleted!");
+                break;
+            }
+        }
+    }
+
+    public void deleteItemMultiple(List<Item> newItemList, String itemName) {
+        boolean itemFound = false;
+        System.out.println("\nMultiple items found with the same name");
+        System.out.println("Input username associated with the item:");
+        String usernameToDelete = input.next();
+        for (Item item : newItemList) {
+            if (item.getItemName().equals(itemName) 
+                        && item.getUsername().equals(usernameToDelete)) {
+                newItemList.remove(item);
+                System.out.println("\nItem successfully deleted!");
+                itemFound = true;
+                break;
+            }
+        }
+        if (!itemFound) {
+            System.out.println("\nItem not found. Please check the item name, username and try again");
+        }
+    }      
+
+     
+    private void generatePassword(List<Item> newItemList) {
+
+    }
+
+
 
 }
